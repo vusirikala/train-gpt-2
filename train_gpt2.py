8268be7f2aa7ba17764cf1f02eea81b17822df6e
vusirikala <satyasatya123456@gmail.com>
2026-04-02 16:52:45 -0700

    Load pretrained weights from huggingface

--- train_gpt2.py
+++ train_gpt2.py
@@ -6,12 +6,11 @@ from torch.nn import functional as F
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
-    dropout: float = 0.0
+    block_size: int = 1024 # maximum sequence length
+    vocab_size: int = 50257 # Number of tokens in the vocabulary: 50,000 BPE merges + 256 bytes tokens + 1 <end of text> token
+    n_layer: int = 12 # Number of layers (Number of attention blocks)
+    n_head: int = 6 # Number of attention heads
+    n_embd: int = 768 # Embedding dimension
 
 class MLP(nn.Module):
     def __init__(self, config: GPTConfig):
@@ -128,3 +127,54 @@ class GPT(nn.Module):
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
         ))
         self.llm_head = nn.Linear(config.n_embd, config.vocab_size)
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 weights from Hugging Face."""
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+
+        from transformers import GPT2LMHeadModel
+        print("Loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embed are determined by the model type
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768), # 124M parameters
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024), # 350M parameters
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280), # 774M parameters
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600), # 1558M parameters
+        }[model_type]
+
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+
+        # create a from-scratch initialized minGPT model
+        # Initialize config with hyperparameters
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")] # Discard this mask / buffer. The ".attn.bias" is just used for auto-regressive masking.
+
+        # init a hunggiface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")] # Ignore these
+
+        # Some of the weights in the model we contructed are transposed when compared to the weight in huggingface
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(suffix) for suffix in transposed):
+                # Special treatment for the Conv1D weights we need to transpose.
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # Vanilla copy over the other parameters.
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
